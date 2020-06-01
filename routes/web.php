28acf20cae945f3fdb783603ce91e3398a1d886e
jy_pauly <jy.pauly.fortfolio@gmail.com>
2020-06-02 01:31:52 +0800

    Added yarn

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $pizzas = ['pizza1', 'pizza2'];
+
+    return view('welcome', compact('pizzas'));
 });
